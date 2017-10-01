3de9f3036e8d118c3baf8fa47cce70da996f5a81
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-10-01 17:36:28 +0530

    Version: Bumped to 0.0.3

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages # pylint: disable=no-name-in-module,import-error
+import os
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = """
+    Beautiful terminal spinners in Python. Find the documentation here: https://github.com/ManrajGrover/halo
+    """
 
 def dependencies(file):
     with open(file) as f:
@@ -11,10 +16,10 @@ def dependencies(file):
 setup(
     name='halo',
     packages=find_packages(exclude=('tests', 'examples')),
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     description='Beautiful terminal spinners in Python',
-    long_description=readme(),
+    long_description=long_description,
     author='Manraj Singh',
     author_email='manrajsinghgrover@gmail.com',
     url='https://github.com/ManrajGrover/halo',
