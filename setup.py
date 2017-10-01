98442c718d46520312cc2bc2a4b2cda80c5a2938
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-10-01 17:58:11 +0530

    Halo: Fix builds

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup, find_packages # pylint: disable=no-name-in-module,import-error
 import os
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    long_description = """
-    Beautiful terminal spinners in Python. Find the documentation here: https://github.com/ManrajGrover/halo
-    """
-
 def dependencies(file):
     with open(file) as f:
         return f.read().splitlines()
@@ -16,10 +8,10 @@ def dependencies(file):
 setup(
     name='halo',
     packages=find_packages(exclude=('tests', 'examples')),
-    version='0.0.3',
+    version='0.0.5',
     license='MIT',
     description='Beautiful terminal spinners in Python',
-    long_description=long_description,
+    long_description='Beautiful terminal spinners in Python. Find the documentation here: https://github.com/ManrajGrover/halo',
     author='Manraj Singh',
     author_email='manrajsinghgrover@gmail.com',
     url='https://github.com/ManrajGrover/halo',
