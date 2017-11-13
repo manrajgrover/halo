becf559e45584994583d46934edcb439291b9f2f
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-11-13 21:18:53 +0530

    Examples: Add context manager example

---
+++ examples/context_manager.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""Examples for halo.
+"""
+import os
+import time
+
+os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from halo import Halo
+
+with Halo(text='Loading', spinner='dots'):
+    # Run time consuming work here
+    time.sleep(4)
+
+with Halo(text='Loading 2', spinner='dots'):
+    # Run time consuming work here
+    time.sleep(4)
