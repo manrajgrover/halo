c5d414b570fd574e43388a27851db3a0ab9849bf
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-12-24 15:14:25 +0530

    Examples: Add example for text wrapping animation

---
+++ examples/long_text.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Examples for halo.
+"""
+from __future__ import unicode_literals, absolute_import, print_function
+import os
+import time
+
+os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from halo import Halo
+
+spinner = Halo(text='This is a text that it is too long. In fact, it exceeds the eighty column standard '
+                    'terminal width, which forces the text frame renderer to add an ellipse at the end of the '
+                    'text. This should definitely make it more than 180!', spinner='dots', animation='marquee')
+
+try:
+    spinner.start()
+    time.sleep(15)
+    spinner.succeed('End!')
+except (KeyboardInterrupt, SystemExit):
+    spinner.stop()
