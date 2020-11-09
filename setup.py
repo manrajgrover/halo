662d1c20792ec52feff248b3bbe0b5102f9ef1b2
Vilhelm Prytz <vilhelm@prytznet.se>
2020-11-09 10:38:09 +0100

    setup.py: Remove Python 3.4 since it's not tested

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     license="MIT",
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
